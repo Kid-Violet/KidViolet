74cc47ac227075684aa3bc14a5cb61a53ee75076
Gaspra <richard@gapsra.co.uk>
2019-05-08 20:36:21 +0100

    cleaning up

--- kidviolet.js
+++ kidviolet.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
-    $("#soundcloud").on('click', function() {
+    $("#Soundcloud").on('click', function() {
+        window.open('https://soundcloud.com/kidviolet', '_blank');
+    });
+
+    $("#Spotify").on('click', function() {
+        window.open('https://soundcloud.com/kidviolet', '_blank');
+    });
+
+    $("#Facebook").on('click', function() {
+        window.open('https://soundcloud.com/kidviolet', '_blank');
+    });
+
+    $("#Twitter").on('click', function() {
         window.open('https://soundcloud.com/kidviolet', '_blank');
     });
 })

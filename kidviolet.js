9409ed7a975763dacae3c5bb4e4b5656a445caf2
Kid-Violet <chazzawilkinson@gmail.com>
2019-05-18 15:16:31 +0100

    Add Teemill Link & Logo

--- kidviolet.js
+++ kidviolet.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
     $("#Soundcloud").on('click', function() {
         window.open('https://soundcloud.com/kidviolet', '_blank');
     });
+	
+	$("#Teemill").on('click', function() {
+        window.open('https://kidviolet.teemill.com/', '_blank');
+    });
 
     $("#Facebook").on('click', function() {
         window.open('https://www.facebook.com/Kid.Violet/', '_blank');

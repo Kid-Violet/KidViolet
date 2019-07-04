0c80ff00f9c86e47e9c376c4c7c119d49dde76c7
Kid-Violet <50734982+Kid-Violet@users.noreply.github.com>
2019-07-04 14:38:03 +0100

    Add Spotify

--- kidviolet.js
+++ kidviolet.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
         window.open('https://soundcloud.com/kidviolet', '_blank');
     });
 	
+    $("#Spotify").on('click', function() {
+        window.open('https://open.spotify.com/artist/18Kupu2OZy7drYCfFlIxfz?si=eAQy4VfJRp2TlNtn0ZXuzw', '_blank');
+    });
+	
 	$("#Teemill").on('click', function() {
         window.open('https://kidviolet.teemill.com/', '_blank');
     });
